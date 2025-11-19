e0c7768160df910ba6c88154e78743810d972f86
Tejpatel610 <tejpatel610@gmail.com>
2025-11-19 13:09:54 -0500

    Temp rename navbar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import Navbar from "./components/navbar";
+import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import Projects from "./components/Projects";
 import ResumeAnalyzer from "./components/ResumeAnalyzer";
